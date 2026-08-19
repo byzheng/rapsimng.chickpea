b73cae7a0fb454c2a3eae2d2ff7de49aa1c3d140
Bangyou Zheng (laptop) <zheng.bangyou@gmail.com>
2026-08-19 20:48:42 +1000

    Add initial project structure and core functionality for chickpea model
    - Create .Rbuildignore and .gitignore files for build and version control
    - Add 404 error page and CNAME for GitHub Pages
    - Implement Contributor Code of Conduct
    - Set up CI workflows for R CMD check and pkgdown
    - Define DESCRIPTION and LICENSE files for package metadata
    - Add core functions for thermal time calculations and options management
    - Create tests for core functionalities
    - Update README with installation instructions and documentation links

---
+++ R/options.R
@@ -0,0 +1,49 @@
+#' chickpea model options manager
+#'
+#' An options manager for configuring chickpea model parameters. This object provides
+#' methods to get and set chickpea related parameters.
+#'
+#' @section Available Options:
+#' \describe{
+#'   \item{phenology.thermal_time.x}{Cardinal temperatures for thermal time calculation. Default: c(2, 30, 35)}
+#'   \item{phenology.thermal_time.y}{Effective thermal time at corresponding cardinal temperatures. Default: c(0, 28, 0)}
+#'   \item{phenology.vernalisation_time.x}{Cardinal temperatures for vernalisation time calculation. Default: c(0, 2, 8, 15)}
+#'   \item{phenology.vernalisation_time.y}{Effective vernalisation time at corresponding cardinal temperatures. Default: c(0, 1, 0.5, 0.0)}
+#' }
+#'
+#' @section Methods:
+#' \describe{
+#'   \item{get(key)}{Retrieve the value of an option by its key (e.g., "phenology.thermal_time.x")}
+#'   \item{set(key, value)}{Set the value of an option by its key}
+#'   \item{reset()}{Reset all options to their default values}
+#' }
+#'
+#' @export
+#' @importFrom optree create_options_manager
+#' @examples
+#' # Get default thermal time cardinal temperatures
+#' chickpea$get("phenology.thermal_time.x")
+#' 
+#' # Get default vernalisation time parameters
+#' chickpea$get("phenology.vernalisation_time.x")
+#' chickpea$get("phenology.vernalisation_time.y")
+#' 
+#' # Set custom values
+#' chickpea$set("phenology.thermal_time.x" = c(0, 25, 40))
+#' chickpea$set("phenology.thermal_time.y" = c(0, 25, 0))
+#' 
+#' # Reset to defaults
+#' chickpea$reset()
+chickpea <- optree::create_options_manager(
+    defaults = list(
+        phenology = list(
+            thermal_time = list(
+                x = c(0, 30, 40),
+                y = c(0, 30, 0)
+            )
+        )
+    ),
+    validators = list(
+        "phenology.thermal_time" = thermaltime_validator
+    )
+)
---
+++ README.md
@@ -0,0 +1,20 @@
+[![R-CMD-check.yaml](https://github.com/byzheng/rapsimng.chickpea/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/byzheng/rapsimng.chickpea/actions/workflows/R-CMD-check.yaml)
+
+# rapsimng.chickpea
+
+chickpea model for APSIM Next Generation.
+
+## Installation
+
+Currently on [Github](https://github.com/byzheng/rapsimng.chickpea) only. Install with:
+
+```r
+remotes::install_github('byzheng/rapsimng.chickpea')
+```
+
+## Documentation
+
+For detailed documentation and reference, visit: https://chickpea.rapsimng.bangyou.me
+
+---
+
