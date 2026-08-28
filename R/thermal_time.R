fc2ba84cd49d9b92aadc256dd4adf25b9c3b0ea3
Bangyou Zheng (laptop) <zheng.bangyou@gmail.com>
2026-08-28 14:38:44 +1000

    Add thermal time method option and update related documentation and tests

--- R/thermal_time.R
+++ R/thermal_time.R
@@ -23,7 +23,7 @@ thermal_time <- function(mint,
     maxt,
     x_temp = chickpea$get("phenology.thermal_time.x"), 
     y_temp = chickpea$get("phenology.thermal_time.y"),
-    method = "HourlySinPpAdjusted",
+    method = chickpea$get("phenology.thermal_time.method"),
     ...) {
     
     tidyweather::thermal_time(
