46ec15f8d79f6aa00f58ea8b8811053c4443343a
Bangyou Zheng (laptop) <zheng.bangyou@gmail.com>
2026-09-14 15:24:36 +1000

    Add return value documentation for chickpea options manager

--- R/options.R
+++ R/options.R
@@ -22,6 +22,7 @@
 #' @export
 #' @importFrom optree create_options_manager
 #' @importFrom optree v_xypair
+#' @return An option manager object for configuring chickpea model parameters.
 #' @examples
 #' # Get default thermal time cardinal temperatures
 #' chickpea$get("phenology.thermal_time.x")
